f78f5549a57b908fd409e313dbbc8b7b5da766ba
CrabAss <jan.hanyu@outlook.com>
2019-07-04 09:35:00 +0100

    add member model

---
+++ DiscordChatExporter.Core.Models/GuildMember.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Tyrrrz.Extensions;
+
+namespace DiscordChatExporter.Core.Models
+{
+    // ...
+
+    public partial class GuildMember
+    {
+        public User User { get; }
+
+        public string Nickname { get; }
+
+        public IReadOnlyList<Role> Roles { get; }
+
+        public DateTimeOffset JoinedAt { get; }
+
+        public DateTimeOffset PremiumSince { get; }
+
+        public bool IsDeaf { get; }
+
+        public bool IsMute { get; }
+
+        public GuildMember(User user, string nickname, IReadOnlyList<Role> roles, DateTimeOffset joinedAt,
+            DateTimeOffset premiumSince, bool isDeaf, bool isMute)
+        {
+            User = user;
+            Nickname = nickname;
+            Roles = roles;
+            JoinedAt = joinedAt;
+            PremiumSince = premiumSince;
+            IsDeaf = isDeaf;
+            IsMute = isMute;
+
+            // ...
+        }
+
+        public override string ToString() => User.FullName;
+    }
+
+    public partial class GuildMember
+    {
+        // ...
+    }
+}
